6c752598b3ef7c66adc82c81cb75a3f446287d69
Joel Ala-aho <joel.ala-aho@netti.fi>
2020-02-27 19:43:01 +0200

    More finishing touches

--- index.js
+++ index.js
@@ -266,5 +266,5 @@ app.post("/listings", passport.authenticate('jwt', { session: false }), (req, re
 app.use("/imageupload", imageupload);
 
 app.listen(port, () => {
-    console.log("Listening to requests on http://localhost:${port}");
+    console.log(`Listening to requests on http://localhost:${port}`);
 });
